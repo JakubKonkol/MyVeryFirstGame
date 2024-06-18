c6789bec3679bb172b2df3f42b73aacceb6125ab
JakubKonkol <s24406@pjwstk.edu.pl>
2024-06-18 11:00:55 +0200

    score&font

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@ ifeq ($(OS),Windows_NT)
 	SDL2_LIB_PATH = enter_path_to_sdl
 	RM = del /q /f
 	EXECUTABLE = game.exe
-	FLAGS = -lmingw32 -lSDL2main -lSDL2
+	FLAGS = -lmingw32 -lSDL2main -lSDL2 -lSDL2_ttf
 	CXXFLAGS += -I$(SDL2_INCLUDE_PATH)
 	LDFLAGS += -L$(SDL2_LIB_PATH)
 # MacOS
@@ -15,7 +15,7 @@ else ifeq ($(shell uname), Darwin)
 	SDL2_LIB_PATH = /usr/local/opt/sdl2/lib
 	RM = rm -f
 	EXECUTABLE = game
-	FLAGS = -lSDL2main -lSDL2
+	FLAGS = -lSDL2main -lSDL2 -lSDL2_ttf
 	CXXFLAGS += -I$(SDL2_INCLUDE_PATH)
 	LDFLAGS += -L$(SDL2_LIB_PATH)
 else
--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
 #include <chrono>
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_ttf.h>
 #include <iostream>
 #include <random>
 #include <thread>
-
 
 #include "src/Player.cpp"
 #include "src/Bullet.cpp"
@@ -15,6 +15,9 @@
 #define SCREEN_HEIGHT 720
 std::vector<Enemy> enemies;
 std::vector<BloodSplash> bloodSplashes;
+int score = 0;
+bool isFullScreen = false;
+
 void spawnEnemyAtRandomCoordinate() {
     int randomX = std::rand() % SCREEN_WIDTH;
     int randomY = std::rand() % SCREEN_HEIGHT;
@@ -22,11 +25,42 @@ void spawnEnemyAtRandomCoordinate() {
     if (randomY > SCREEN_HEIGHT - 50) randomY = SCREEN_HEIGHT - 50;
     enemies.push_back(Enemy(randomX, randomY));
 }
-int main(int argc, char** argv){
 
-    /*
-     * General initialization
-     */
+void renderText(SDL_Renderer* renderer, TTF_Font* font, const std::string& text, SDL_Color color, int x, int y) {
+    SDL_Surface* surface = TTF_RenderText_Solid(font, text.c_str(), color);
+    SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
+    SDL_Rect destRect = { x, y, surface->w, surface->h };
+    SDL_FreeSurface(surface);
+    SDL_RenderCopy(renderer, texture, nullptr, &destRect);
+    SDL_DestroyTexture(texture);
+}
+
+void setRendererSize(SDL_Renderer* renderer, int width, int height) {
+    SDL_RenderSetLogicalSize(renderer, width, height);
+}
+
+// BROKEN
+void toggleFullScreen(SDL_Window* window, SDL_Renderer* renderer) {
+    Uint32 flags = SDL_GetWindowFlags(window);
+    if (flags & SDL_WINDOW_FULLSCREEN) {
+        SDL_SetWindowFullscreen(window, 0);
+        SDL_SetWindowSize(window, SCREEN_WIDTH, SCREEN_HEIGHT);
+        setRendererSize(renderer, SCREEN_WIDTH, SCREEN_HEIGHT);
+        isFullScreen = false;
+    } else {
+        SDL_SetWindowFullscreen(window, SDL_WINDOW_FULLSCREEN);
+        int w, h;
+        SDL_GetWindowSize(window, &w, &h);
+        setRendererSize(renderer, w, h);
+        isFullScreen = true;
+    }
+}
+
+std::string getScore() {
+    return "SCORE: " + std::to_string(score);
+}
+
+int main(int argc, char** argv) {
     std::cout << "starting game..." << std::endl;
     SDL_Window *window;
     SDL_Renderer *renderer;
@@ -37,9 +71,13 @@ int main(int argc, char** argv){
     Uint32 lastTime = SDL_GetTicks();
     bool running = true;
 
-
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
         SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't initialize SDL: %s", SDL_GetError());
+        return 3;
+    }
+
+    if (TTF_Init() == -1) {
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't initialize TTF: %s", TTF_GetError());
         return 3;
     }
 
@@ -47,27 +85,33 @@ int main(int argc, char** argv){
         SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't create window and renderer: %s", SDL_GetError());
         return 3;
     }
-    // GAME LOOP
-    while(running){
 
+    SDL_Color black = { 0, 0, 0 };
+    TTF_Font* font32 = TTF_OpenFont("resources/font.ttf", 32);
+
+    if (!font32) {
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Failed to load font: %s", TTF_GetError());
+        return 3;
+    }
+
+    // GAME LOOP
+    while (running) {
         SDL_Event event;
-        while(SDL_PollEvent(&event)){
-            switch(event.type){
+        while (SDL_PollEvent(&event)) {
+            switch (event.type) {
                 case SDL_QUIT:
                     running = false;
-                break;
+                    break;
                 case SDL_KEYUP:
+                    // if (event.key.keysym.scancode == SDL_SCANCODE_F) toggleFullScreen(window, renderer);
                     if (event.key.keysym.scancode == SDL_SCANCODE_ESCAPE) running = false;
-                break;
-
+                    break;
                 default:
                     player.handleEvent(event);
                     break;
             }
         }
-        /*
-         * Calculating delta time
-         */
+
         Uint32 currentTime = SDL_GetTicks();
         Uint32 deltaTime = currentTime - lastTime;
         lastTime = currentTime;
@@ -78,43 +122,42 @@ int main(int argc, char** argv){
             timeSinceLastSpawn = 0;
         }
 
-        // DEBUG
-        // std::cout << "Game time: " << gameTime << std::endl;
         player.update(SCREEN_WIDTH, SCREEN_HEIGHT, dt, enemies);
         SDL_SetRenderDrawColor(renderer, 0, 255, 255, 255);
         SDL_RenderClear(renderer);
         player.render(renderer);
+        renderText(renderer, font32, getScore(), black, SCREEN_WIDTH / 2 - 50, 100);
 
-        /*
-         * Renderowanie przeciwnikow i sprawdzenie kolizji z pociskami
-         */
         for (auto& enemy : enemies) {
             enemy.render(renderer);
-
-            for(auto& bullets : player.bullets){
-                if(enemy.checkBulletCollision(bullets.mRect)){
+            for (auto& bullets : player.bullets) {
+                if (enemy.checkBulletCollision(bullets.mRect)) {
                     enemy.isHit = true;
+                    score++;
                 }
             }
-            if(enemy.isHit) {
+            if (enemy.isHit) {
                 BloodSplash bs(enemy.mRect.x, enemy.mRect.y);
                 bloodSplashes.push_back(bs);
-
                 enemy.isHit = false;
             }
         }
+
         for (auto& bs : bloodSplashes) {
             bs.render(renderer);
             bs.update(dt);
         }
 
         SDL_RenderPresent(renderer);
-        // Delays
-        if (deltaTime < 1000/60) {
-            SDL_Delay((1000/60) - deltaTime);
+
+        if (deltaTime < 1000 / 60) {
+            SDL_Delay((1000 / 60) - deltaTime);
         }
     }
-    std::cout << "Quiting game...." << std::endl;
+
+    std::cout << "Quitting game..." << std::endl;
+    TTF_CloseFont(font32);
+    TTF_Quit();
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
